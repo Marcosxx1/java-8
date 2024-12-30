5a13ae3a7efef385549af377740940b48e1ad973
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-12-30 14:45:42 -0400

    Numeric streams example

--- src/main/java/com/technical/numericstreams/NumericStreamsExample.java
+++ src/main/java/com/technical/numericstreams/NumericStreamsExample.java
@@ -1,4 +1,30 @@
 package com.technical.numericstreams;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 public class NumericStreamsExample {
+
+    public static int sumOfNNumbers(List<Integer> integerList) {
+        return integerList.stream()
+                .reduce(0, (firstElement, secondElement) -> firstElement + secondElement); // Converte Integer para int
+
+        /*      .reduce(Integer::sum); //Optional<Integer> retorno
+                .reduce((firstElement, secondElement) -> Integer.sum(firstElement, secondElement)); */
+    }
+
+    public static int sumOfNNumbersgIntStream(){
+        return IntStream.rangeClosed(1, 6 )  // Quando formos realizar operações relacionadas a números, podemos utilizar o IntStream
+                .sum();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        System.out.println(sumOfNNumbers(integerList));
+        System.out.println(sumOfNNumbersgIntStream());
+    }
+
 }
